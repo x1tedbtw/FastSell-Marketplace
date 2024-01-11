242f076b0e27212b5fd86ebd1f8c282c89073c49
Antorof1 <antorof@proton.me>
2024-01-11 17:14:55 +0100

    Removed outdated instruction

--- backend/store/settings.py
+++ backend/store/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from decouple import config
 
-config.read_env(".env")
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
