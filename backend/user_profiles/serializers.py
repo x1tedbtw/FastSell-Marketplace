6a7baa9ae97359f17a81e7b29ea7ae3b14d6235d
Antorof1 <antorof@proton.me>
2024-01-15 15:25:54 +0100

    Fixed UserProfileSerializer

--- backend/user_profiles/serializers.py
+++ backend/user_profiles/serializers.py
@@ -4,5 +4,5 @@ from .models import UserProfile
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
-        fields = ["username", "first_name", "last_name", "email", "password", "location", "phone_number"]
+        fields = ["id", "username", "first_name", "last_name", "email", "password", "location", "phone_number"]
         extra_kwargs = {"password": {"write_only": True}}
