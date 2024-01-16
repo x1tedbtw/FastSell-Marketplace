88818072c1428aa4adb7b4e291a23e78b3bcf9d2
Antorof1 <antorof@proton.me>
2024-01-16 16:58:16 +0100

    Pushed django secret key to env vars

--- backend/store/settings.py
+++ backend/store/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5ke^qvk+fxb4hun%(xmu-18%t*5=l3o(5ji@bmw-hmb$w*frqb'
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
