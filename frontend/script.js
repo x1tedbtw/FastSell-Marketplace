911317000584767a43ada059eff965066cd2f893
x1tedbtw <nefedov.vitali@gmail.com>
2024-01-16 12:13:56 +0100

    Implementation of frontend files (index.html,style.css, script.js )

---
+++ frontend/script.js
@@ -0,0 +1,8 @@
+function performSearch() {
+        // Get the value from the search input
+    const searchTerm = document.getElementById("search-input").value;
+
+    // Perform some action with the search term, e.g., redirect to search results page
+        // For demonstration purposes, let's just log the term to the console
+        console.log("Search term: " + searchTerm);
+    }
