33c02dd2eb59f27450b5f26037dff02af1eb357a
andrewhusach <agusach@gmail.com>
2024-01-16 16:41:48 +0100

    Small error fix

--- backend/offers/urls.py
+++ backend/offers/urls.py
@@ -5,7 +5,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
     path("", OfferList.as_view()),
     path("<int:pk>/", OfferDetail.as_view()),
-    path("categories/", CategoryList.as_view())
+    path("categories/", CategoryList.as_view()),
     path("categories/<int:category_pk>/subcategories/", SubcategoryList.as_view())
 ]
 
