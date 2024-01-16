74c4d9dbda65b9c517abbf90d4c089b4b10790df
Antorof1 <antorof@proton.me>
2024-01-16 17:36:09 +0100

    Made category names unique value

--- backend/offers/models.py
+++ backend/offers/models.py
@@ -4,7 +4,7 @@ from user_profiles.models import UserProfile
 
 class Category(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
@@ -13,7 +13,7 @@ class Category(models.Model):
 class Subcategory(models.Model):
     id = models.AutoField(primary_key=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
