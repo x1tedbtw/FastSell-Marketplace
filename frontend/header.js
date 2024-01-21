6ab1f9eb3319121e31adcf363904082bdb411007
Antorof1 <antorof@proton.me>
2024-01-21 19:45:38 +0100

    Made header dynamic to authorization

---
+++ frontend/header.js
@@ -0,0 +1,15 @@
+import { validateToken } from "./auth.js";
+
+async function main() {
+    if (!validateToken());
+    const header_first_button = document.getElementById("header-button1");
+    const header_second_button = document.getElementById("header-button2");
+
+    header_first_button.innerHTML = "Create Offer";
+    header_first_button.setAttribute("href", "createoffer.html");
+
+    header_second_button.innerHTML = "My Account";
+    header_second_button.setAttribute("href", "myprofile.html");
+}
+
+document.addEventListener("DOMContentLoaded", main);
