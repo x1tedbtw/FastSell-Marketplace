0de671486c8d5f045821e605e6e30b7af1e4cd24
Antorof1 <antorof@proton.me>
2024-01-22 22:52:45 +0100

    Some small title fixes

--- frontend/scripts/createoffer.js
+++ frontend/scripts/createoffer.js
@@ -29,38 +29,3 @@ function saveOffer(){
         })
 
 }
-
-
-function populateSubcategories() {
-        var categorySelect = document.getElementById("category");
-        var subcategorySelect = document.getElementById("subcategory");
-        subcategorySelect.innerHTML = "";
-
-        switch (categorySelect.value) {
-            case "category1":
-                addOption(subcategorySelect, "subcategory1", "Motor Subcategory 1");
-                addOption(subcategorySelect, "subcategory2", "Motor Subcategory 2");
-                addOption(subcategorySelect, "subcategory3", "Motor Subcategory 3");
-                break;
-            case "category2":
-                addOption(subcategorySelect, "subcategory4", "Real Estate Subcategory 1");
-                addOption(subcategorySelect, "subcategory5", "Real Estate Subcategory 2");
-                addOption(subcategorySelect, "subcategory6", "Real Estate Subcategory 3");
-                addOption(subcategorySelect, "subcategory7", "Real Estate Subcategory 4");
-                break;
-            case "category3":
-                addOption(subcategorySelect, "subcategory8", "Home subcategoty")
-            default:/* DOBAWLYAEM SUDA CATEGORII TOWAROW*/
-                break;
-        }
-    }
-
-    function addOption(select, value, text) {
-        var option = document.createElement("option");
-        option.value = value;
-        option.text = text;
-        select.add(option);
-    }
-
-
-    populateSubcategories();
