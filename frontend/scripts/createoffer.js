e7f18389cba2efe472b98379ff5f938b25be020a
OleksandrPaslavskyi <paslavskijs00@gmail.com>
2024-01-22 22:01:09 +0100

    Created Offer function

--- frontend/scripts/createoffer.js
+++ frontend/scripts/createoffer.js
@@ -1,5 +1,36 @@
 import "./components/AppHeader.js";
 
+
+function main() {
+    document.getElementById("submit-button").addEventListener("click", saveOffer)
+}
+
+function saveOffer(){
+    const create_offer_form = document.getElementById("createOfferForm");
+    const formData = new FormData(create_offer_form);
+
+    const post_data = {};
+
+    formData.forEach((value, key) => {
+        post_data[key] = value;
+
+    });
+
+    axios.post("/api/offers/", post_data, {
+        headers: {
+            "Authorization":`Token ${getToken()} `
+        }
+    })
+        .then((response) =>{
+            console.log(response.data)
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+
+}
+
+
 function populateSubcategories() {
         var categorySelect = document.getElementById("category");
         var subcategorySelect = document.getElementById("subcategory");
