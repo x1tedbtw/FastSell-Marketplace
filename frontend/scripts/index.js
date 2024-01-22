32ab4e770dd1906a0779540a1e6229995942bcce
Antorof1 <antorof@proton.me>
2024-01-22 21:33:47 +0100

    Simple fetch offers function by categories

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -1,1 +1,16 @@
 import "./components/AppHeader.js";
+
+
+function main() {
+    getOffers()
+    .then((data) => {
+        console.log(data);
+    })
+}
+
+async function getOffers(category = null) {
+    const category_url = category ? `?category=${category}` : "";
+    return (await axios.get(`/api/offers/${category_url}`)).data;
+}
+
+document.addEventListener("DOMContentLoaded", main);
