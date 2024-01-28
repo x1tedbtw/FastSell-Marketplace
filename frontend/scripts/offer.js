7458812bdd48da74a05c17648e104072cc8210a6
x1tedbtw <nefedov.vitali@gmail.com>
2024-01-28 21:25:50 +0100

    Created "information" section about owner of the offer(offer.html, offer.css), also added functional part(offer.js)

--- frontend/scripts/offer.js
+++ frontend/scripts/offer.js
@@ -27,11 +27,19 @@ function getOfferDetails(offerId) {
 function displayOfferDetails(offerData) {
     const offerImage = document.querySelector(".offer-image");
     const productName = document.querySelector(".product-name");
+    const fistName = document.querySelector(".first-name")
+    const secondName = document.querySelector(".second-name")
+    const email = document.querySelector(".email")
+    const address = document.querySelector(".address")
     const price = document.querySelector(".price");
     const description = document.getElementById("product-description");
 
     offerImage.src = offerData.images[0].image;
     productName.textContent = offerData.title;
+    fistName.textContent = offerData.fistName
+    secondName.textContent = offerData.secondName
+    email.textContent = offerData.email
+    address.textContent = offerData.address
     price.textContent = `Price: ${offerData.price} zł`;
     description.textContent = offerData.description;
 }
